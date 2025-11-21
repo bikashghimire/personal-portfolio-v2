a0d5e5ca43d3db24073d7a92dd2039d12e9d9e1c
Bikash Ghimire <bikashghimire2610@gmail.com>
2025-11-21 21:59:18 +0200

    fix:linting error

--- src/test/utils/test-utils.tsx
+++ src/test/utils/test-utils.tsx
@@ -4,6 +4,7 @@ import { LanguageProvider } from '@/contexts/LanguageContext';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 // Custom render function that includes providers
+// eslint-disable-next-line react-refresh/only-export-components
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <LanguageProvider>
@@ -19,6 +20,7 @@ const customRender = (
   options?: Omit<RenderOptions, 'wrapper'>,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
+// eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react';
 export { customRender as render };
 
